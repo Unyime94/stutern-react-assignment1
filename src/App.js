1595d0f51df0a556c5cf9940b2f3518b926f896f
unyime <unyimeusen01@gmail.com>
2022-06-18 18:26:37 +0100

    added all project files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './Header.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='header-class'>Basic React Application</h1>
+    </div>
+  );
+}
+
+export default App;
